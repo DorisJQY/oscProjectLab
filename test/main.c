e33f401c96b2d88cb526d341d57e5dbbd37a86d0
qianyao jiang <qianyao.jiang@student.kuleuven.be>
2024-12-19 11:08:48 +0100

    sbuffer test

---
+++ test/main.c
@@ -0,0 +1,190 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "config.h"
+#include <semaphore.h>
+#include "sbuffer.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+FILE* binFile;
+FILE* dataFile;
+FILE* storageFile;
+sbuffer_t* sbuffer;
+
+void *writer_thread(void *arg)
+{
+  while(1)
+  {
+    if(binFile == NULL) break;
+
+    //read from sensor_data single value by single value
+    uint16_t sensor_id;
+    double temperature;
+    time_t timestamp;
+
+    if(fread(&sensor_id, sizeof(uint16_t), 1, binFile) != 1)
+    {
+      if(feof(binFile))
+      {
+        sensor_id = 0;
+      }
+      else
+      {
+        printf("Error reading sensor_id.\n");
+        break;
+      }
+    }
+
+    if(sensor_id == 0)
+    {
+      sensor_data_t end_marker = {0, 0.0, 0};
+      sbuffer_insert(sbuffer, &end_marker);
+
+      printf("End marker created by writer.\n");
+      break;
+    }
+
+    if(fread(&temperature, sizeof(double), 1, binFile) != 1)
+    {
+      printf("Error reading temperature.\n");
+      break;
+    }
+
+    if(fread(&timestamp, sizeof(time_t), 1, binFile) != 1)
+    {
+      printf("Error reading timestamp.\n");
+      break;
+    }
+
+    sensor_data_t data;
+    data.id = sensor_id;
+    data.value = temperature;
+    data.ts = timestamp;
+
+    //write to buffer
+    if(sbuffer_insert(sbuffer, &data) == SBUFFER_FAILURE)
+    {
+      printf("Write to buffer failed.\n");
+      break;
+    }
+
+  }
+  return NULL;
+}
+
+void *data_thread(void *arg)
+{
+  while(1)
+  {
+    if(dataFile == NULL) break;
+    //read from buffer
+    sensor_data_t data;
+    int result = sbuffer_remove(sbuffer, &data);
+    if (result == SBUFFER_NO_DATA)
+    {
+      break;
+    }
+    else if (result == SBUFFER_FAILURE)
+    {
+      printf("Read from buffer failed.\n");
+      break;
+    }
+
+    //write to sensor_data_out.csv
+    if(dataFile != NULL)
+    {
+      fprintf(dataFile, "%u, %.6f, %ld\n", data.id, data.value, data.ts);
+      fflush(dataFile);
+    }
+
+  }
+  return NULL;
+}
+
+void *storage_thread(void *arg)
+{
+  while(1)
+  {
+    if(storageFile == NULL) break;
+    //read from buffer
+    sensor_data_t data;
+    int result = sbuffer_remove(sbuffer, &data);
+    if (result == SBUFFER_NO_DATA)
+    {
+      break;
+    }
+    else if (result == SBUFFER_FAILURE)
+    {
+      printf("Read from buffer failed.\n");
+      break;
+    }
+
+    //write to sensor_data_out.csv
+    if(storageFile != NULL)
+    {
+      fprintf(storageFile, "%u, %.6f, %ld\n", data.id, data.value, data.ts);
+      fflush(storageFile);
+    }
+  }
+  return NULL;
+}
+
+int main()
+{
+  binFile = fopen("sensor_data", "rb");
+  if(!binFile)
+  {
+    perror("Failed to open sensor_data");
+    return -1;
+  }
+
+  dataFile = fopen("data_out.csv", "a");
+  if(!dataFile)
+  {
+    perror("Failed to open data_out.csv");
+    fclose(binFile);
+    return -1;
+  }
+
+  storageFile = fopen("storage_out.csv", "a");
+  if(!storageFile)
+  {
+    perror("Failed to open storage_out.csv");
+    fclose(binFile);
+    fclose(dataFile);
+    return -1;
+  }
+
+  if(sbuffer_init(&sbuffer) == -1)
+  {
+    printf("Create buffer failed.\n");
+    return -1;
+  }
+
+  pthread_t writer, reader1, reader2;
+
+  pthread_create(&writer, NULL, writer_thread, NULL);
+  pthread_create(&reader1, NULL, data_thread, NULL);
+  pthread_create(&reader2, NULL, storage_thread, NULL);
+
+  pthread_join(writer, NULL);
+  pthread_join(reader1, NULL);
+  pthread_join(reader2, NULL);
+
+  fclose(binFile);
+  binFile = NULL;
+  fclose(dataFile);
+  dataFile = NULL;
+  fclose(storageFile);
+  storageFile = NULL;
+
+  if(sbuffer_free(&sbuffer) == -1)
+  {
+    printf("Free buffer failed.\n");
+    return -1;
+  }
+
+  printf("Program completed successfully.\n");
+  return 0;
+}
---
+++ test/sbuffer.c
@@ -0,0 +1,150 @@
+/**
+ * \author {AUTHOR}
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include "sbuffer.h"
+#include "config.h"
+/**
+ * basic node for the buffer, these nodes are linked together to create the buffer
+ */
+typedef struct sbuffer_node {
+    struct sbuffer_node *next;  /**< a pointer to the next node*/
+    sensor_data_t data;         /**< a structure containing the data */
+    int count;
+} sbuffer_node_t;
+
+/**
+ * a structure to keep track of the buffer
+ */
+struct sbuffer {
+    sbuffer_node_t *head;       /**< a pointer to the first node in the buffer */
+    sbuffer_node_t *tail;       /**< a pointer to the last node in the buffer */
+};
+
+pthread_mutex_t bufferMut;
+pthread_mutex_t endMut;
+pthread_cond_t bufferCond;
+bool done = false;
+
+int sbuffer_init(sbuffer_t **buffer) {
+    *buffer = malloc(sizeof(sbuffer_t));
+    if (*buffer == NULL) return SBUFFER_FAILURE;
+    (*buffer)->head = NULL;
+    (*buffer)->tail = NULL;
+
+    if (pthread_mutex_init(&bufferMut, NULL) != 0)
+    {
+      free(*buffer);
+      return SBUFFER_FAILURE;
+    }
+    if (pthread_mutex_init(&endMut, NULL) != 0)
+    {
+      pthread_mutex_destroy(&bufferMut);
+      free(*buffer);
+      return SBUFFER_FAILURE;
+    }
+    if (pthread_cond_init(&bufferCond, NULL) != 0)
+    {
+      pthread_mutex_destroy(&endMut);
+      pthread_mutex_destroy(&bufferMut);
+      free(*buffer);
+      return SBUFFER_FAILURE;
+    }
+
+    return SBUFFER_SUCCESS;
+}
+
+int sbuffer_free(sbuffer_t **buffer) {
+    sbuffer_node_t *dummy;
+    if ((buffer == NULL) || (*buffer == NULL)) {
+        return SBUFFER_FAILURE;
+    }
+
+    while ((*buffer)->head) {
+        dummy = (*buffer)->head;
+        (*buffer)->head = (*buffer)->head->next;
+        free(dummy);
+    }
+    free(*buffer);
+    *buffer = NULL;
+
+    if (pthread_mutex_destroy(&bufferMut) != 0) return SBUFFER_FAILURE;
+    if (pthread_cond_destroy(&bufferCond) != 0) return SBUFFER_FAILURE;
+    if (pthread_mutex_destroy(&endMut) != 0) return SBUFFER_FAILURE;
+
+    return SBUFFER_SUCCESS;
+}
+
+int sbuffer_remove(sbuffer_t *buffer, sensor_data_t *data) {
+    sbuffer_node_t *dummy;
+    if (buffer == NULL) return SBUFFER_FAILURE;
+
+    pthread_mutex_lock(&bufferMut);
+    while (buffer->head == NULL && !done)
+    {
+      pthread_cond_wait(&bufferCond, &bufferMut);
+    }
+    if (buffer->head == NULL && done)
+    {
+      printf("End marker detected by reader. Exiting.\n");
+      pthread_mutex_unlock(&bufferMut);
+      return SBUFFER_NO_DATA;
+    }
+
+    *data = buffer->head->data;
+    dummy = buffer->head;
+    dummy->count--;
+    if (dummy->count == 0)
+    {
+      if (buffer->head == buffer->tail) // buffer has only one node
+      {
+        buffer->head = buffer->tail = NULL;
+      } 
+      else  // buffer has many nodes empty
+      {
+        buffer->head = buffer->head->next;
+      }
+      free(dummy);
+    }
+
+    if (data->id == 0)
+    {
+      printf("End marker detected by reader. Exiting.\n");
+      done = true;
+      pthread_cond_signal(&bufferCond);
+      pthread_mutex_unlock(&bufferMut);
+      return SBUFFER_NO_DATA;
+    }
+    pthread_cond_signal(&bufferCond);
+    pthread_mutex_unlock(&bufferMut);
+
+    return SBUFFER_SUCCESS;
+}
+
+int sbuffer_insert(sbuffer_t *buffer, sensor_data_t *data) {
+    sbuffer_node_t *dummy;
+    if (buffer == NULL) return SBUFFER_FAILURE;
+    dummy = malloc(sizeof(sbuffer_node_t));
+    if (dummy == NULL) return SBUFFER_FAILURE;
+
+    pthread_mutex_lock(&bufferMut);
+    dummy->data = *data;
+    dummy->next = NULL;
+    dummy->count = 2;
+    if (buffer->tail == NULL) // buffer empty (buffer->head should also be NULL
+    {
+        buffer->head = buffer->tail = dummy;
+    } else // buffer not empty
+    {
+        buffer->tail->next = dummy;
+        buffer->tail = buffer->tail->next;
+    }
+    pthread_cond_signal(&bufferCond);
+    pthread_mutex_unlock(&bufferMut);
+
+    return SBUFFER_SUCCESS;
+}
